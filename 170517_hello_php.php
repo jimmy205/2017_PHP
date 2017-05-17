0346c71f67dbc69fc60bc1deda2d39c6a51c8fbd
jimmy <k4545452@gmail.com>
2017-05-17 16:35:00 +0800

    20170517_hello_php

---
+++ 170517_hello_php.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/5/17
+ * Time: 下午 03:57
+ */
+
+echo 'Hello!Php<br>';
+echo 'Hello!Php<br>';
+
+$abc=2;
+$_abc=2.5;
+$____=true;
+$A23='str';
+
+echo gettype($abc);
+echo '<br>';
+var_dump($_abc);
+?>
+<hr/>
+<h1>Hello!Php</h1>
