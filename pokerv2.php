a9c3d6c622ed3816dbf998105d040c0bcf737577
JimmyLion <k4545452@gmail.com>
2017-05-23 00:50:08 +0800

    05222017

--- pokerv2.php
+++ pokerv2.php
@@ -1,18 +1,23 @@
 <?php
 
-for($i=0;$i<10;$i++) $check[]=fales;
+for($i=0;$i<10;$i++) $check[]=false ;
+
+// echo $check[9];
+
 
 for($i=0;$i<10;$i++){
 
     do{
-        $isRepeat=false;
         $temp=rand(0,9);
+        $isRepeat=false;
         if(!$check[$temp]){
             $poker[]=$temp;
             $check[$temp]=true;
+           // echo $temp;
+           // break;
         }else{
-            $isRepeat=true;
+           $isRepeat=true;
         }
     }while($isRepeat);
-    echo $poker[$i] . '<br>';
+    echo $poker[$i] . '<br>' ;
 }
