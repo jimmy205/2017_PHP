f7a058fd31b880efdba58af1698279fb4760c3b0
JimmyLion <k4545452@gmail.com>
2017-05-22 21:14:25 +0800

    05222017

--- 170519.php
+++ 170519.php
@@ -31,11 +31,11 @@ if (isset($_GET['x'])) {
 <form>
 
     <input type="text" name="x" id="x" value="<?php echo $x ?>"></input>
-    <select name="selected" >
-        <option value="plus" <?php if ($Operator=="plus"){echo "selected";}?>>+</option>
-        <option value="minus" <?php if ($Operator=="minus"){echo "selected";}?>>-</option>
-        <option value="times" <?php if ($Operator=="times"){echo "selected";}?>>x</option>
-        <option value="divided" <?php if ($Operator=="divided"){echo "selected";}?>>/</option>
+    <select name="selected" id="selected">
+        <option  value="plus" <?php if ($Operator=="plus"){echo "selected";}?>>+</option>
+        <option  value="minus" <?php if ($Operator=="minus"){echo "selected";}?>>-</option>
+        <option  value="times" <?php if ($Operator=="times"){echo "selected";}?>>x</option>
+        <option  value="divided" <?php if ($Operator=="divided"){echo "selected";}?>>/</option>
     </select>
     <input type="text" name="y" id="y" value="<?php echo $y ?>"></<input>
 <input type="submit" id="btn" value="=" </input>
@@ -43,16 +43,34 @@ if (isset($_GET['x'])) {
 
 </form>
 
-<!--<input type="button" value="script" onclick="cal()"/>-->
-<!---->
-<!--<input type="text" name="x" id="xx"></input>-->
-<!---->
-<!--<script>-->
-<!---->
-<!--        function cal() {-->
-<!--             var x=document.getElementById("x").value;-->
-<!--            alert(x);-->
-<!--        }-->
-<!---->
-<!---->
-<!--</script>-->
+<input type="button" value="script" onclick="cal()"/>
+
+
+
+<script>
+
+        function cal() {
+             var x=document.getElementById("x").value;
+             var y=document.getElementById("y").value;
+             var x2=parseInt(x);
+             var y2=parseInt(y);
+             var op=selected.value;
+             //alert(op);
+            if(op =='plus'){
+                alert(x2+y2);
+            }else if(op=='minus'){
+                alert(x2-y2);
+            }else if(op=='times'){
+                alert(x2*y2);
+            }else if(op=='divided' && x2 % y2 != 0){
+                var xy=x2/y2;
+                var xy2=x2%y2;
+
+                alert(Math.floor(xy)+'...'+xy2);
+            }else{
+                alert(x2/y2);
+            }
+        }
+
+
+</script>
--- 170519_3_if.php
+++ 170519_3_if.php
@@ -1,7 +1,23 @@
-<?php
-$sc = 1600;
-echo $sc . '<br>';
+<form>
+    <input type="text" id="year" name="year"/>
+    <input type="submit" value="count" />
+</form>
 
-if ($sc % 4 == 0 ){
-    echo "ok";
+<?php
+$sc = $_GET['year'];
+echo "Your Year = {$sc} <br>" ;
+
+if($sc != "") {
+    if (($sc % 400 == 0) || $sc % 4 == 0 && $sc % 100 != 0) {
+        echo "Leap Year";
+    } else {
+        echo "Not Leap Year";
+    }
+}else{
+    echo '<script> alert("Enter a Year") </script>' ;
 }
+?>
+
+
+
+
